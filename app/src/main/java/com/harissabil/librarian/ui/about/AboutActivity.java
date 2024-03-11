1710d4f5b84a56f79c587fc608eaefd2f0982077
Haris <harissabil04@gmail.com>
2024-03-11 15:26:34 +0700

    Feat: Add about screen

---
+++ app/src/main/java/com/harissabil/librarian/ui/about/AboutActivity.java
@@ -0,0 +1,74 @@
+package com.harissabil.librarian.ui.about;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.splashscreen.SplashScreen;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.harissabil.librarian.BuildConfig;
+import com.harissabil.librarian.R;
+import com.harissabil.librarian.databinding.ActivityAboutBinding;
+
+import java.util.Objects;
+
+public class AboutActivity extends AppCompatActivity {
+
+    private ActivityAboutBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SplashScreen.installSplashScreen(this);
+        binding = ActivityAboutBinding.inflate(getLayoutInflater());
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return WindowInsetsCompat.CONSUMED;
+        });
+
+        setSupportActionBar(binding.toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        try {
+            Glide.with(this)
+                    .load(getPackageManager().getApplicationIcon("com.harissabil.librarian"))
+                    .into(binding.ivLogo);
+        } catch (PackageManager.NameNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        binding.tvVersion.setText(getString(R.string.version, BuildConfig.VERSION_NAME));
+
+        binding.cvSourceCode.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/harissabil/Librarian"));
+            startActivity(intent);
+        });
+
+        binding.cvPrivacy.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/harissabil/GLogsPrivacyPolicy"));
+            startActivity(intent);
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            getOnBackPressedDispatcher().onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
