a78cc291979331321ae0d89d4ca4a28d86c97720
Haris <harissabil04@gmail.com>
2024-03-12 14:04:39 +0700

    Feat: Add Dagger Hilt dependency injection

---
+++ app/src/main/java/com/harissabil/librarian/LibrarianApp.java
@@ -0,0 +1,9 @@
+package com.harissabil.librarian;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class LibrarianApp extends Application {
+}
